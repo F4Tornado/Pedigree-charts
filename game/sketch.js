a0fb3ea022a322b81b24c347ce3879cc4b5ef797
F4Tornado <h.rovnyak@gmail.com>
2018-02-09 12:44:22 -0500

    hehehe

--- game/sketch.js
+++ game/sketch.js
@@ -2,8 +2,12 @@ let people = [];
 
 function setup() {
   createCanvas(600, 600);
+  people.push(new person("male", color(51), 10, 10))
 }
 
 function draw() {
   fill(235);
+  for (person of people) {
+    person.show();
+  }
 }
