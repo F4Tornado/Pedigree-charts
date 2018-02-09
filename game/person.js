2a2408050df06ca8177494523c3196cc2a93fd5d
F4Tornado <h.rovnyak@gmail.com>
2018-02-09 12:53:40 -0500

    Using the fly swatter

--- game/person.js
+++ game/person.js
@@ -1,24 +1,18 @@
-class Person {
-  this.gender;
-  this.color;
-  this.children = [];
-  this.x;
-  this.y;
-  constructor(gender, color, x, y) {
-    this.gender = gender;
-    this.color = color;
-    this.x = x;
-    this.y = y;
-  }
-  show() {
+function Person(gender, color, x, y) {
+  this.size = 20;
+  this.gender = gender;
+  this.color = color;
+  this.x = x;
+  this.y = y;
+  this.show = () => {
     if (gender === "male") {
       stroke(51);
       fill(color);
-      rect(this.x, this.y, 10, 10);
+      rect(this.x, this.y, this.size, this.size);
     } else {
       stroke(51);
       fill(color);
-      ellipse(this.x, this.y, 10, 10);
+      ellipse(this.x, this.y, this.size, this.size);
     }
   }
 }
