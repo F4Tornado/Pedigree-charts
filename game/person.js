76636e2c2b5263d275679bc0a138f524f113dd4f
F4Tornado <h.rovnyak@gmail.com>
2018-02-22 10:36:53 -0500

    Genotypes

--- game/person.js
+++ game/person.js
@@ -1,15 +1,21 @@
-function Person(gender, color, x, y, depth) {
+function Person(gender, genotype, x, y, depth) {
   this.size = 20;
   this.gender = gender;
-  this.color = color;
+  this.genotype = genotype;
+  this.phenotype = getPhenotype(this.genotype);
+  if (this.phenotype == "dom") {
+    this.color = color(51);
+  } else {
+    this.color = color(235);
+  }
   this.x = x;
   this.y = y;
   this.textBox = createInput();
   this.depth = depth;
   this.married;
-  for (let i = 0; i < random([1, 2, 3, 4]); i++) {
-    // children.push = Person(random(["male", "female"]), random([color(51), color(235)]));
-  }
+  // for (let i = 0; i < random([1, 2, 3, 4]); i++) {
+  //   children.push = Person(random(["male", "female"]), random([color(51), color(235)]));
+  // }
   this.show = () => {
     stroke(114, 168, 255);
     strokeWeight(2);
@@ -18,12 +24,12 @@ function Person(gender, color, x, y, depth) {
     line(this.x+this.size, this.y+(this.size/2), people[this.married].x+this.size, people[this.married].y+(this.size/2));
     if (gender === "male") {
       stroke(51);
-      fill(color);
+      fill(this.color);
       rect(this.x, this.y, this.size, this.size);
     } else {
       ellipseMode(CORNER);
       stroke(51);
-      fill(color);
+      fill(this.color);
       ellipse(this.x, this.y, this.size, this.size);
     }
     this.textBox.position(this.x+5, this.y+30).elt.maxLength = 2;
--- game/sketch.js
+++ game/sketch.js
@@ -4,10 +4,10 @@ let maxDepth = 3;
 function setup() {
   smooth();
   createCanvas(600, 600);
-  people.push(new Person("male", color(51), 10, 10, 1));
-  people.push(new Person("female", color(51), 200, 10, 1));
-  people.push(new Person("male", color(235), 390, 10, 1));
-  people.push(new Person("female", color(235), 570, 10, 1));
+  people.push(new Person("male", "Aa", 10, 10, 1));
+  people.push(new Person("female", "AA", 200, 10, 1));
+  people.push(new Person("male", "aa", 390, 10, 1));
+  people.push(new Person("female", "aa", 570, 10, 1));
   people[0].married = 1;
   people[1].married = 0;
   people[2].married = 3;
@@ -20,3 +20,23 @@ function draw() {
     person.show();
   }
 }
+
+const getPhenotype = function(genotype) {
+  if (genotype.toLowerCase() == genotype) {
+    return "rec";
+  } else {
+    return "dom";
+  }
+}
+
+// const scribbleRect = (x, y, w, h) => {
+//   scribble.scribbleRect(x, y, w, h);
+// }
+//
+// const scribbleEllipse = (x, y, w, h) => {
+//   scribble.scribbleRect(x, y, w, h);
+// }
+//
+// const scribbleLine = (x1, y1, x2, y2) => {
+//   scribble.scribbleLine(x1, y1, x2, y2);
+// }
